c67018cca24ad1ae3c4b15e538ac2ffd370d33ec
T-PRAT <Titouan.p@hotmail.fr>
2026-02-03 20:02:30 +0100

    Update WorkoutView.vue

--- src/views/WorkoutView.vue
+++ src/views/WorkoutView.vue
@@ -779,7 +779,7 @@ const rightSlots = computed(() => [
           >
             <svg class="w-3 h-3 md:w-4 md:h-4" fill="currentColor" viewBox="0 0 20 20">
               <path
-                fill-rule="evenodd",
+                fill-rule="evenodd"
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zM8 7a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1V8a1 1 0 00-1-1H8z"
                 clip-rule="evenodd"
               />
